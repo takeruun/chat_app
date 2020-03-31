c4aaa95622dc4357706b7438e468782c28cfd588
takeruun <hstake25@gmail.com>
2020-03-31 21:01:35 +0900

    user model controller create

---
+++ back/db/migrate/20200331105618_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password
+      t.string :icon
+      t.boolean :is_login
+      t.string :token
+
+      t.timestamps
+    end
+  end
+end
