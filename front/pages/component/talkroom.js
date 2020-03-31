5db7b491192108bb4b7784078808a411fd3df9d4
takeruun <hstake25@gmail.com>
2020-03-31 18:52:59 +0900

    talkroom ページ追加

---
+++ front/pages/component/talkroom.js
@@ -0,0 +1,20 @@
+import { Component } from "react";
+import request from 'superagent';
+
+export default class talkRoom extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      currentChatMessage: '',
+      chatLogs: [],
+      users: [],
+      user_id: '',
+      partner_id: '',
+      room_name: '',
+      room_id: '',
+      change_talk: false
+    }
+  };
+}
+
+apiGet
