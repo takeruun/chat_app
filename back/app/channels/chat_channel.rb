6528681b9076df2921b4b8c3ae1f0b6d47931689
takeruun <hstake25@gmail.com>
2020-04-03 14:32:39 +0900

    チャンネル ブロードキャスト

---
+++ back/app/channels/chat_channel.rb
@@ -0,0 +1,17 @@
+class ChatChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "chat_channel_#{params[:room_id]}"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def create(data)
+    Message.create(
+      user: User.find(data.fetch('user_id')),
+      room: Room.find(params[:room_id]),
+      body: data.fetch('body')
+      )
+  end
+end
