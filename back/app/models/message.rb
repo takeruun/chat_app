79e3707daed7c1e1b02b63d4db16afe52e9d2db8
takeruun <hstake25@gmail.com>
2020-04-03 14:30:51 +0900

    各モデル関係付け

--- back/app/models/message.rb
+++ back/app/models/message.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class Message < ApplicationRecord
+  belongs_to :user
+  belongs_to :room
+
+  after_create_commit do 
+    ChatMessageCreationEventBroadcastJob.perform_later(self)
+  end
 end
