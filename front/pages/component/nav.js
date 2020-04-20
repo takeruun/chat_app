b8672bdfd3bcb95fcf8840b5fe17d8b0149cb33e
takeruun <hstake25@gmail.com>
2020-04-20 15:16:09 +0900

    eslint修正

--- front/pages/component/nav.js
+++ front/pages/component/nav.js
@@ -1,68 +1,76 @@
-import Link from 'next/link'
+import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSignInAlt, faHome, faUser, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
-import React, {Component} from 'react'
-import Router from 'next/router'
+import {
+  faSignInAlt,
+  faHome,
+  faUser,
+  faSignOutAlt,
+} from '@fortawesome/free-solid-svg-icons';
+import React, { Component } from 'react';
 import request from 'superagent';
 export default class Nav extends Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
   }
 
-  componentDidMount(){
-  }
-    
+  componentDidMount() {}
+
   logout() {
-    request
-      .post('/api/logout')
-      .end((err, res) => {
-        if(err){
-          console.log(err);
-        }
-        if(res.body){
+    request.post('/api/logout').end((err, res) => {
+      if (err) {
+        console.log(err);
+      }
+      if (res.body) {
         console.log(res.body.text);
         window.location.reload();
-        }
-      })
+      }
+    });
   }
 
-  render () {
+  render() {
     return (
-    <nav className='nav'>
-      <ul>
-        <li>
-          <Link href="/login">
-            <span><FontAwesomeIcon icon={ faSignInAlt } /></span>
-          </Link>
-        </li>
-        <li>
-          <Link href="/">
-            <span><FontAwesomeIcon icon={ faHome } /></span>
-          </Link>
-        </li>
-        {(()=> {
-          if (this.props.userId){
-            return(
-            <li onClick={e　=> this.logout()}>
-              <span><FontAwesomeIcon icon={ faSignOutAlt } /></span>
-            </li>
-            )
-          }
-          else{
-            return(
-            <li>
-              <Link href="/user">
-                <span><FontAwesomeIcon icon={ faUser } /></span>
-              </Link>
-            </li>
-            )
-          }
-        })()}
-      </ul>
+      <nav className="nav">
+        <ul>
+          <li>
+            <Link href="/login">
+              <span>
+                <FontAwesomeIcon icon={faSignInAlt} />
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <span>
+                <FontAwesomeIcon icon={faHome} />
+              </span>
+            </Link>
+          </li>
+          {(() => {
+            if (this.props.userId) {
+              return (
+                <li onClick={(e) => this.logout()}>
+                  <span>
+                    <FontAwesomeIcon icon={faSignOutAlt} />
+                  </span>
+                </li>
+              );
+            } else {
+              return (
+                <li>
+                  <Link href="/user">
+                    <span>
+                      <FontAwesomeIcon icon={faUser} />
+                    </span>
+                  </Link>
+                </li>
+              );
+            }
+          })()}
+        </ul>
         <style jsx>{`
           nav {
             padding: 16px;
-            background-color: #262F42;
+            background-color: #262f42;
             width: 80px;
             height: 100vh;
           }
@@ -80,20 +88,20 @@ export default class Nav extends Component {
           span {
             width: 40px;
             height: 40px;
-            background-color: #ECF0F4;
+            background-color: #ecf0f4;
             border-radius: 40px;
             display: flex;
             align-items: center;
             justify-content: center;
           }
-          span :hover{
+          span :hover {
             cursor: pointer;
           }
           svg {
-            background-color: #ECF0F4;
+            background-color: #ecf0f4;
           }
         `}</style>
       </nav>
-    )
+    );
   }
 }
